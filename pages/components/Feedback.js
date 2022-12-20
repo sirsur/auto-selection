455604a7d71e63a58be161a4698d492b7916c966
deanny <casimowa.c2001@gmail.com>
2022-12-20 13:51:20 +0300

    feedback carousel, style and media ready

--- pages/components/Feedback.js
+++ pages/components/Feedback.js
@@ -1,17 +1,34 @@
 import styles from '../../styles/component.module.css';
-import { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { useMediaQuery } from "@material-ui/core";
 
 const feedbacks = [
-    'Привет я тестовый запрос номер 0. Желаю хорошего дня',
-    'Привет я тестовый запрос номер 1. Пиздец как ты меня уже заебал крутить',
-    'Привет я тестовый запрос номер 2. Программист ты конечно да',
-    'Привет я тестовый запрос номер 3. Поскорее бы отдохнуть',
-    'Привет я тестовый запрос номер 4. Скоро новый год',
-    'Привет я тестовый запрос номер 5. Это какой-то ужас а не браузерные события',
+    'Очень доволен, что при продаже автомобиля обратился именно к вам. Оперативно приехали, сделали фотографии, реально оценили техническое состояние. Помогли продать Toyota Camry в кратчайшие сроки.',
+    'Продавала авто после ДТП. Все быстро оформили. А главное дали отличную цену. Спасибо большое.',
+    'Решила продать свой форд. Среди множества конкурентов, данная компания предложила больше всех. Осталась довольна сотрудничеством и рекомендую другим автовладельцам.',
+    'Работают оперативно! Нужны были деньги предложил свое авто. Рассчитали в тот же день и помогли продать как можно быстрее.',
+    'Продал ребятам свой автомобиль, нужны были срочно деньги, автосалоны предлагали только обмен на новый авто, а тут все быстро, от звонка до написания договора меньше часа и я довольный с деньгами, советую.',
+    'Парень, который работал со мной - молодец, работает четко! Самое главное вежливый и доброжелательный. Расчет сразу без задержки!',
 ];
 
+const variants = {
+    desktop: {
+        y: ['calc(0*37vh)', 'calc(1*37vh)', 'calc(2*37vh)', 'calc(3*37vh)', 'calc(4*37vh)', 'calc(5*37vh)', 'calc(6*37vh)']
+    },
+    tablet: {
+        x: ['calc(0*63vw)', 'calc(1*63vw)', 'calc(2*63vw)', 'calc(3*63vw)', 'calc(4*63vw)', 'calc(5*63vw)', 'calc(6*63vw)'],
+        y: [0, 0, 0, 0, 0, 0, 0]
+    },
+    mobile: {
+        x: ['calc(0*67vw)', 'calc(1*67vw)', 'calc(2*67vw)', 'calc(3*67vw)', 'calc(4*67vw)', 'calc(5*67vw)', 'calc(6*67vw)'],
+        y: [0, 0, 0, 0, 0, 0, 0]
+    }
+}
+
 export default function Feedback() {
+    let isDesktop = useMediaQuery("(max-width:1024px)");
+    let isMobile = useMediaQuery("(max-width:750px)");
+
     return (
         <section id='feedback'>
             <div>
@@ -19,43 +36,42 @@ export default function Feedback() {
                 <h1>100</h1>
                 <h2>положительных отзывов</h2>
             </div>
-            <AnimatePresence>
-                <motion.div className={styles.carousel}>
-                        <motion.div className={styles.carousel_item}>
-                            <p>{feedbacks[0]}</p>
-                        </motion.div>
-                </motion.div>
-            </AnimatePresence>
-            {
-                /*
-            <motion.div
-                className={styles.carousel}
-                animate={{ y: '37vh' }}
-                transition={{ delay: 5, repeat: Infinity, duration: 1, repeatDelay: 5 }}
+            <motion.div 
+                className={styles.carousel} 
+                animate={
+                    isMobile ? "mobile" : (isDesktop ? "tablet" : "desktop")
+                }
+                transition={{ 
+                    repeat: Infinity, 
+                    ease: "backInOut",
+                    duration: 40
+                }}
+                variants={variants}
             >
-                <div className={styles.carousel_item_prev_prev}>
-                    { prevPrevSlide }
-                </div>
                 <motion.div 
-                    className={styles.carousel_item_prev}
-                    animate={{ filter: 'blur(0px)' }}
-                    transition={{ delay: 5, repeat: Infinity, duration: 1, repeatDelay: 5 }}
+                    className={ styles.carousel_item }
                 >
-                    { prevSlide }
+                    <p>{feedbacks[feedbacks.length - 2]}</p>
                 </motion.div>
                 <motion.div 
-                    className={styles.carousel_item_cur}
-                    animate={{ filter: 'blur(7px)' }}
-                    transition={{ delay: 5, repeat: Infinity, duration: 1, repeatDelay: 5 }}
+                    className={ styles.carousel_item }
                 >
-                    { curSlide }
+                    <p>{feedbacks[feedbacks.length - 1]}</p>
                 </motion.div>
-                <div className={styles.carousel_item_next}>
-                    { nextSlide }
-                </div>
+                {feedbacks?.map((feedback, index) => 
+                    <motion.div 
+                        key={ index }
+                        className={ styles.carousel_item }
+                    >
+                        <p>{feedback}</p>
+                    </motion.div>
+                )}
+                <motion.div 
+                    className={ styles.carousel_item }
+                >
+                    <p>{feedbacks[0]}</p>
+                </motion.div>
             </motion.div>
-            */
-            }
         </section>
     )
 };
