4831fa48573a59aa402a052896af7eeecc426dfd
deanny <casimowa.c2001@gmail.com>
2022-12-23 09:14:13 +0300

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="shortcut icon" href="../public/favicon.ico" />
             </Head>
             <style jsx global>{`
                 html {
